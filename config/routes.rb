e70ccdfb7b4a63cbfadec7db3bb3b58789ad7f11
Leslie Kornes <lesliekornes@gmail.com>
2023-01-06 12:48:34 -0500

    add nested review resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :reviews
   root "movies#index"
 
   # get "/movies" => "movies#index"
@@ -8,6 +7,8 @@ Rails.application.routes.draw do
   # get "/movies/:id/edit" => "movies#edit", as: "edit_movie"
   # patch "/movies/:id" => "movies#update"
   
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 
 end
