4b4622911eb9180497df5836dc274bf919629f17
Leslie Kornes <lesliekornes@gmail.com>
2023-01-17 17:07:59 -0500

    add review helper method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,13 @@
 module ReviewsHelper
+
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, "No reviews")
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1), 'star')
+      
+      # asterisk/star option below, but i like the decimal pt value used above :shrug:
+      # "*" * movie.average_stars.round
+    end
+  end
 end
