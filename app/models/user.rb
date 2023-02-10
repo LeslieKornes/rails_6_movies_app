edd807eb6c1a80db42307322bfc145311131d60a
Leslie Kornes <lesliekornes@gmail.com>
2023-02-10 13:46:23 -0500

    add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 #  email           :string
 #  name            :string
 #  password_digest :string
+#  username        :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
@@ -13,6 +14,9 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
+  validates :username, presence: true,
+                       format: { with: /\A[A-Z0-9]+\z/i },
+                       uniqueness: { case_sensitive: false }
 
   validates :email, format: { with: /\S+@\S+/ },
                     uniqueness: { case_sensitive: false }
