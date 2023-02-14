46b9c143c8ff8c057d797b6061bc71f17ae373c9
Leslie Kornes <lesliekornes@gmail.com>
2023-02-14 10:49:15 -0500

    add sign_up route and html5 required for session form that does not have backing model or validations

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/sign_up" => "users#new"
 
   resource :session, only: [:new, :create, :destroy]
+  get "/sign_in" => "sessions#new"
   
   resources :movies do
     resources :reviews
