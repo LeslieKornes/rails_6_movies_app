dfed095faf2f981305035c99242db63180168647
Leslie Kornes <lesliekornes@gmail.com>
2023-02-21 09:18:59 -0500

    make session login be avail on either email or username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:email])
+    # fail
+    user = User.find_by(email: params[:email_or_username]) ||
+      User.find_by(username: params[:email_or_username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.name}"
