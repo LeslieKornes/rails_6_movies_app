d64d282427619ffe77c7cb2b1b97fe20c946a257
Leslie Kornes <lesliekornes@gmail.com>
2023-03-03 09:30:29 -0500

    restrict edit and delete to current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index 
     @users = User.all
@@ -49,5 +50,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_correct_user
+    redirect_to :root unless current_user?(@user)
   end
 end
