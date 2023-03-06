6499a288fa6e951779c301c3748ee9e40e808425
Leslie Kornes <lesliekornes@gmail.com>
2023-03-06 11:26:00 -0500

    add intended_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def require_signin
     unless current_user
+      session[:intended_url] = request.url
       redirect_to new_session_url, notice: "you have to sign in!"
     end
   end
