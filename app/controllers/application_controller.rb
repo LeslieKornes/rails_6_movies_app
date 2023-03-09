4375ab2232fa33119abe02a40fe2dd17ae982ad6
Leslie Kornes <lesliekornes@gmail.com>
2023-03-09 09:28:33 -0500

    add admin to users and restrict certain actions accordingly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,15 @@ class ApplicationController < ActionController::Base
     current_user == user
   end
 
-  helper_method :current_user, :current_user?
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
+
+  helper_method :current_user, :current_user?, :current_user_admin?
 end
